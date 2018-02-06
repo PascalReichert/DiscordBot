8b35a006610f31f44905a213b6e2156d836bbbff
PascalReichert <32631847+PascalReichert@users.noreply.github.com>
2018-02-06 12:37:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,55 @@
 var Discord = require('discord.js');
+var economy = require('discord-eco');
 var bot = new Discord.Client();
 var fs = require('fs');
+var moment = require('moment');
 
-var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
+var userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
 
+//functions
+
+
+//Listener Event: Message Received (this will run every time a message is received)
 bot.on('message', message => {
-    
+
+  var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
+
+    //Variables
     var sender = message.author; //the person who sent the message
     var msg = message.content.toUpperCase(); //takes message and makes it all uppercase
-    var prefix = '!'
+    var prefix = '!' //the text before commands
     var cont = message.content.slice(prefix.length).split(" ");
     var args = cont.slice(1);
-    
+
+    if (sender.id === '410348764788817920'){
+      return;
+    }
+
+    fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
+      if(err) console.error(err);
+    })
+
+    //help command
     if (msg === prefix + 'HELP' || msg === prefix + 'COMMANDS')
     {
       message.channel.send(commandsList)
     }
-    
+
+    //ping/pong command
     if(msg === prefix + 'PING')
     {
         message.channel.send('Pong! Ping = **' + bot.ping + '**')
     }
-}
+});
 
+//Listener even: Bot Launched
 bot.on('ready', () => {
     console.log('Bot Launched...')
 
     bot.user.setStatus('online')
-    bot.user.setActivity('fontys.nl/uitschrijven')
+    bot.user.setGame('fontys.nl/uitschrijven')
 
 });
 
+// THIS  MUST  BE  THIS  WAY
 bot.login(process.env.BOT_TOKEN);
