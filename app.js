35724b5cbc084e550c54c8dd667a1554ab6d352e
PascalReichert <32631847+PascalReichert@users.noreply.github.com>
2018-02-06 12:01:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
+bot.on('message', message => {
+    
+    var sender = message.author; //the person who sent the message
+    var msg = message.content.toUpperCase(); //takes message and makes it all uppercase
+    var prefix = '!'
+    var cont = message.content.slice(prefix.length).split(" ");
+    var args = cont.slice(1);
+    
+    if(msg === prefix + 'PING')
+    {
+        message.channel.send('Pong! Ping = **' + bot.ping + '**')
+    }
 
 bot.on('ready', () => {
     console.log('Bot Launched...')
